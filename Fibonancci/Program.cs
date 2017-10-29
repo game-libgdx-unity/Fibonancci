19a0ce15f0e8f053ebf9d49e07f36519a1556365
Thanh Vinh Vu <mrthanhvinh168@gmail.com>
2017-10-29 20:15:05 +0700

    Initial commit

---
+++ Fibonancci/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonancci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum: {0}", Fibonancci());
+            Console.ReadLine();
+        }
+        static ulong Fibonancci()
+        {
+            ulong result = 0, n1 = 0, n2 = 1;
+            ulong sum = 0;
+
+            while (result <= 4000000)
+            {
+                result = n1 + n2;
+
+                if (result > 4000000)
+                {
+                    break;
+                }
+
+                if (result % 2 == 0 && result < 4000000)
+                {
+                    sum += result;
+                }
+
+                n1 = n2;
+                n2 = result;
+
+            }
+            return sum;
+        }
+    }
+}
